21e5068d04c79af2cb3a87b348224d38cf257d33
khadiza21 <bibikhadiza474@gmail.com>
2022-01-28 16:34:11 +0600

    hackerrank problem solving

---
+++ CPP/EASY/dAtAtYpes.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    cin >> i >> l >> c >> f >> d;
+    
+    cout<<i<<endl<<l<<endl<<c<<endl<<fixed<<setprecision(3)<<f<<endl<<setprecision(9)<<d<<endl;
+
+    
+
+    return 0;
+}
---
+++ CPP/EASY/iNpUtOuTpUt.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    
+    int a,b,c,n; 
+    cin>>a>>b>>c;
+    n=a+b+c;
+    cout<<n<<endl;
+    return 0;
+}
